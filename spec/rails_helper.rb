b76197823beb052153f014497a010793f939259d
Andre Oliveira <andreoliveirar2d2@gmail.com>
2026-04-27 00:53:24 -0300

    feat: add devcontainers, and configure spec to work with container

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -81,7 +81,7 @@ RSpec.configure do |config|
 
   config.before(:each, type: :system) do
     # driven_by :rack_test
-    driven_by :selenium_chrome # only for debugging purpose
+    # driven_by :selenium_chrome # only for debugging purpose
     # driven_by :selenium_chrome_headless # still working to run on container
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,6 +45,7 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   config.before(:suite) do
+    DatabaseCleaner.allow_remote_database_url = true if Rails.env.test?
     DatabaseCleaner.clean
   end
 
