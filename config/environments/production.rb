934c3a2e4aec2540262e8bb49d63ba55265c44d4
Andre Oliveira <andreoliveirar2d2@gmail.com>
2026-05-29 14:28:55 -0300

    use the actual domain host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,12 +58,12 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: ENV.fetch("SPOTSBY_HOST") } unless ENV["SECRET_KEY_BASE_DUMMY"]
+  config.action_mailer.default_url_options = { host: ENV.fetch("SPOTSBY_DOMAIN") } unless ENV["SECRET_KEY_BASE_DUMMY"]
 
   # Set host for url_for / *_url helpers used outside an HTTP request (jobs,
   # Turbo broadcasts, ActiveStorage url_for(blob)). Without this, url helpers
   # in broadcasts fall back to "www.example.com".
-  Rails.application.routes.default_url_options[:host] = ENV.fetch("SPOTSBY_HOST") unless ENV["SECRET_KEY_BASE_DUMMY"]
+  Rails.application.routes.default_url_options[:host] = ENV.fetch("SPOTSBY_DOMAIN") unless ENV["SECRET_KEY_BASE_DUMMY"]
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
